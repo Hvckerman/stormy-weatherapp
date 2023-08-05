57f1b1a8bc805a15e5e215c5d76056e764f9f075
Hvckerman <youreagle8@gmail.com>
2023-08-05 19:33:59 +0500

    tested on mobile here

--- lib/FrontPage.dart
+++ lib/FrontPage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart'; // Add this import for geocoding
 
 class FrontPage extends StatefulWidget {
-  const FrontPage({super.key});
+  const FrontPage({Key? key}) : super(key: key);
 
   @override
   State createState() => FrontPageState();
@@ -29,29 +31,6 @@ class FrontPageState extends State<FrontPage> {
   String? currentLocation;
 
   TextEditingController _searchController = TextEditingController();
-
-  void _handleSearchSubmit(String input) {
-    _updateCityName(input);
-
-    setState(() {
-      _isSearchMode = false;
-    });
-  }
-
-  void _updateCityName(String input) {
-    setState(() {
-      cityName = input;
-      currentLocation = cityName;
-    });
-    _fetchWeatherData(); // Fetch weather data for the new city name
-  }
-
-  final Icon searchIcon = Icon(Icons.search);
-  final Icon submitIcon = Icon(Icons.check);
-
-  String apiKey =
-      '36e59374a03c4a1d9fd141844232807'; // Replace with your actual API key
-  String cityName = 'Karachi';
 
   Future<void> _fetchWeatherData() async {
     try {
@@ -79,10 +58,68 @@ class FrontPageState extends State<FrontPage> {
     }
   }
 
+  void _handleSearchSubmit(String input) {
+    _updateCityName(input);
+
+    setState(() {
+      _isSearchMode = false;
+    });
+  }
+
+  void _updateCityName(String input) {
+    setState(() {
+      cityName = input;
+      currentLocation = cityName;
+    });
+    _fetchWeatherData(); // Fetch weather data for the new city name
+  }
+
+  final Icon searchIcon = Icon(Icons.search);
+  final Icon submitIcon = Icon(Icons.check);
+
+  String apiKey =
+      '36e59374a03c4a1d9fd141844232807'; // Replace with your actual API key
+  String cityName = 'Karachi';
+
   @override
   void initState() {
     super.initState();
     _fetchWeatherData();
+    _fetchUserLocation(); // Fetch user's current location
+  }
+
+  void _fetchUserLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        // Handle if the user denies location permission
+        print('Location permission denied.');
+        return;
+      }
+
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.medium,
+      );
+
+      // Use Geocoding to get the location name from latitude and longitude
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      if (placemarks.isNotEmpty) {
+        String locationName =
+            '${placemarks.first.locality}, ${placemarks.first.country}';
+        setState(() {
+          currentLocation = locationName;
+          if (cityName == 'Karachi') {
+            cityName = locationName;
+          }
+        });
+        _fetchWeatherData(); // Fetch weather data for the user's location
+      }
+    } catch (e) {
+      print('Error getting user location: $e');
+    }
   }
 
   @override
@@ -138,8 +175,8 @@ class FrontPageState extends State<FrontPage> {
                         children: [
                           if (currentLocation != null)
                             Text(
-                              '$currentLocation',
-                              style: TextStyle(
+                              'Current Location: $currentLocation',
+                              style: const TextStyle(
                                   fontSize: 16, fontWeight: FontWeight.bold),
                             ),
                           Text('Temperature: ${_weatherData!.temperature} °C'),
@@ -148,7 +185,7 @@ class FrontPageState extends State<FrontPage> {
                           Image.network(_weatherData!.iconUrl),
                         ],
                       )
-                    : CircularProgressIndicator(),
+                    : const CircularProgressIndicator(),
               ),
             ),
             Container(
